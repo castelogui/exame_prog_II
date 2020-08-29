65bee88494c3f37e9aa17a61dd189ba00e3e05f0
castelogui <guilhermecastelo.mail@gmail.com>
2020-08-28 20:16:34 -0400

    update messages conexão

--- database/conexao.py
+++ database/conexao.py
@@ -4,12 +4,12 @@ from mysql.connector import errorcode
 
 try:
 	db_connection = mysql.connector.connect(host='localhost', user='root', password='admin', database='ExameFinal')
-	print("Database connection made!")
+	print("Database conectado com sucesso!")
 except mysql.connector.Error as error:
 	if error.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database doesn't exist")
+		print("Database não existe")
 	elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("User name or password is wrong")
+		print("Username ou password estão erradas")
 	else:
 		print(error)
 else:
